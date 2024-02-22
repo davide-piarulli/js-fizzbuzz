55e4b57338221af20fb7dbfd4808b40b198413e3
davide-piarulli <davide.piarulli.1@gmail.com>
2024-02-22 15:57:16 +0000

    tentativo di scrittura in html, non funzionante

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
+const dpcontainer = document.getElementById('dp-container');
 
+// const box = 100;
 
-for(let i = 1; i <= 100; i++){
+for(let i = 1; i <= box; i++){
+
+  const box = document.createElement('div');
+  box.innerHTML = `${i}`
+  box.classList.add('box');
+  dpcontainer.append('box');
 
   if (i % 3 === 0 && i % 5 === 0) {
-    console.log('FizzBuzz');
+    box.innerHTML = 'FizzBuzz';
+    // console.log('FizzBuzz');
   }else if(i % 3 === 0){ 
-    console.log('Fizz');
+    box.innerHTML = 'Fizz';
+    // console.log('Fizz');
   } else if (i % 5 === 0){
-    console.log('Buzz');
+    box.innerHTML = 'Buzz';
+    // console.log('Buzz');
   } else {
-    console.log(i);
+    box.innerHTML = `${i}`
+    // console.log(i);
   }
 }
   
