6d35712e0cb7bc38d68aa627cc294379a7899ff5
davide-piarulli <davide.piarulli.1@gmail.com>
2024-02-22 15:18:47 +0000

    milestone console ok

--- js/script.js
+++ js/script.js
@@ -2,16 +2,18 @@
 
 for(let i = 1; i <= 100; i++){
 
-  //  if(i % 3 === 0){ 
-      
-      console.log(i);
-    // }
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log('FizzBuzz');
+  }else if(i % 3 === 0){ 
+    console.log('Fizz');
+  } else if (i % 5 === 0){
+    console.log('Buzz');
+  } else {
+    console.log(i);
   }
+}
   
   
 
 
 
-
-  // const li = document.createElement('li');
-  // li.innerHTML = `Elemento: ${i}`
